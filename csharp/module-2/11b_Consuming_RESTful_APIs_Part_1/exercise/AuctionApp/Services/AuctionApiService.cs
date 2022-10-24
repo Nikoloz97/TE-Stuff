7dcfb26be959ea20b4de49b7c7b75593f935025d
Nick Gotsy <nick.gotsy@gmail.com>
2022-10-24 15:33:57 -0400

    completed HW

--- csharp/module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/AuctionApp/Services/AuctionApiService.cs
+++ csharp/module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/AuctionApp/Services/AuctionApiService.cs
@@ -1,6 +1,7 @@
 ﻿using RestSharp;
 using System.Collections.Generic;
 using AuctionApp.Models;
+using System.Net.Http;
 
 namespace AuctionApp.Services
 {
@@ -15,22 +16,51 @@ namespace AuctionApp.Services
 
         public List<Auction> GetAllAuctions()
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest("auctions");
+            IRestResponse <List <Auction>> response = client.Get<List<Auction>>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong!");
+            }
+
+            return response.Data;
+
         }
 
         public Auction GetDetailsForAuction(int auctionId)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions/{auctionId}");
+            IRestResponse<Auction> response = client.Get<Auction>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong!");
+            }
+
+            return response.Data;
         }
 
         public List<Auction> GetAuctionsSearchTitle(string searchTerm)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions?title_like={searchTerm}");
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong!");
+            }
+
+            return response.Data;
         }
 
         public List<Auction> GetAuctionsSearchPrice(double searchPrice)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions?currentBid_lte={searchPrice}");
+            IRestResponse<List<Auction>> response = client.Get<List<Auction>>(request);
+            if (!response.IsSuccessful)
+            {
+                throw new HttpRequestException("Something went wrong!");
+            }
+
+            return response.Data;
         }
     }
 }
